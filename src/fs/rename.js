00245ab54a7b12c83ff4f5d805b6718a9b8361bf
NikitaKakurin <neyasut87@rambler.ru>
2022-06-03 20:12:11 +0300

    fix: import and indents in fs

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,8 +1,6 @@
 import fs from 'fs/promises';
 import path from 'path';
-import { fileURLToPath, pathToFileURL } from 'url';
-
-
+import { fileURLToPath } from 'url';
 
 export const rename = async () => {
   const __filename = fileURLToPath(import.meta.url);
