46d9262c2a4269f48e9dd2c4c0af8b64fb275d0c
NikitaKakurin <neyasut87@rambler.ru>
2022-06-03 16:57:17 +0300

    feat: done copy.js with use fs.cp

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,21 @@
+import { mkdir } from 'fs';
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 export const copy = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const sourceFolderPath = path.join(__dirname, 'files');
+    const targetFolderPath = path.join(__dirname, 'files_copy');
+
+    try{
+      await fs.mkdir(targetFolderPath);
+      await fs.access(sourceFolderPath);     
+    } catch(err){
+        throw new Error('FS operation failed');
+    }
+    await fs.cp(sourceFolderPath, targetFolderPath, {recursive:true});
 };
+
+copy();
