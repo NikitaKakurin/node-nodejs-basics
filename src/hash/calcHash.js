8e468a0bbd717943107e67e332c734f378f89fc5
NikitaKakurin <neyasut87@rambler.ru>
2022-06-04 16:33:37 +0300

    feat: done calcHash.js with events

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,21 @@
+import fs from 'fs';
+import { pipeline } from 'stream';
+import crypto from 'crypto';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 export const calculateHash = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const pathToFile = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+  const readableStream = fs.createReadStream(pathToFile, 'utf-8');
+  let data = '';
+  readableStream.on('data', (chunk) =>{
+    data += chunk.toString();
+  })
+  readableStream.on('end', (chunk) =>{
+    const hash = crypto.createHash('sha256').update(data).digest('hex');
+    console.log(hash)
+  })
 };
+calculateHash()
