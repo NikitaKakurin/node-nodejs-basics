43bff4fa24de73f090e46cdd15fadbbf382d8082
NikitaKakurin <neyasut87@rambler.ru>
2022-06-04 18:43:47 +0300

    feat: done decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,29 @@
+import fs from 'fs';
+import zlib from 'zlib';
+import { pipeline } from 'stream';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 export const decompress = async () => {
-    // Write your code here 
-};
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const pathToFile = path.join(__dirname, 'files', 'fileToCompress.txt');
+  const pathToArchive = path.join(__dirname, 'files', 'archive.gz');
+
+  const readableStream = fs.createReadStream(pathToArchive);
+  const writeableStream = fs.createWriteStream(pathToFile);
+  const unzip = zlib.createUnzip();
+
+  pipeline(
+      readableStream,
+      unzip,
+      writeableStream,
+      (err) => {
+        if (err) {
+          console.error('Pipeline failed.', err);
+        }
+      }
+    )
+}
+
+decompress();
