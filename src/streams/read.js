21c687fbcdd76f4b92a40db054a6480566b678c2
NikitaKakurin <neyasut87@rambler.ru>
2022-06-04 08:29:25 +0300

    feat: done streams/read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,22 @@
+import fs from 'fs';
+import { pipeline } from 'stream';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 export const read = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const pathToFile = path.join(__dirname, 'files', 'fileToRead.txt');
+
+  pipeline(
+    fs.createReadStream(pathToFile,'utf-8'),
+    process.stdout,
+    (err) => {
+      if (err) {
+        console.error('Pipeline failed.', err);
+      } 
+    }
+  )
 };
+
+read();
