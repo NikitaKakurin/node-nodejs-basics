76420f2d6101bb7ac0806d69e059e7cad8a02fa1
NikitaKakurin <neyasut87@rambler.ru>
2022-06-04 07:39:44 +0300

    refactor: indents in args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -2,9 +2,9 @@ export const parseArgs = () => {
   let out = [];
   let pattern = new RegExp('^--[A-Za-z_]+[0-9]*');
   for(let i = 0; i < process.argv.length; i++){
-      if(pattern.test(process.argv[i])){
-          out.push(`${process.argv[i].slice(2)} is ${process.argv[i+1]}`);
-      }
+    if(pattern.test(process.argv[i])){
+      out.push(`${process.argv[i].slice(2)} is ${process.argv[i+1]}`);
+    }
   }
   console.log(out.join(', '))
 };
