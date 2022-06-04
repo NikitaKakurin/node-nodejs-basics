9c6818dd7fc87a7509b61ea34db9c45441f63f52
NikitaKakurin <neyasut87@rambler.ru>
2022-06-04 05:57:43 +0300

    refactor: cjsToESm

--- src/modules/cjsToEsm.mjs
+++ src/modules/cjsToEsm.mjs
@@ -1,16 +1,22 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { release, version } from 'os';
+import { createServer as createServerHttp } from 'http';
+import './files/c.js';
+import aJson from './files/a.json' assert {type: 'json'};
+import bJson from './files/b.json' assert {type: 'json'};
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const random = Math.random();
 
 let unknownObject;
 
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
+    unknownObject = aJson;
 } else {
-    unknownObject = require('./files/b.json');
+    unknownObject = bJson;
 }
 
 console.log(`Release ${release()}`);
@@ -24,7 +30,7 @@ const createMyServer = createServerHttp((_, res) => {
     res.end('Request accepted');
 });
 
-module.exports = {
+export {
     unknownObject,
     createMyServer,
 };
