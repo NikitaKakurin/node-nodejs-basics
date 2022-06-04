0aaf44bd308cc3500d8762aa31b635ef0509dc01
NikitaKakurin <neyasut87@rambler.ru>
2022-06-04 11:44:03 +0300

    feat: done transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,34 @@
+import fs from 'fs';
+import { Transform, pipeline } from 'stream';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { reverse } from 'dns';
+
 export const transform = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const reversTransform = new Transform({
+    transform(chunk, enc, cb ){
+      const shortChunk = chunk.toString().trim()
+      const reverseShortChunk = shortChunk.split('').reverse().join('');
+      this.push( `${reverseShortChunk}\n` );
+      cb()
+    }
+  })
+  
+  pipeline(
+    process.stdin,
+    reversTransform,
+    process.stdout,
+    (err) => {
+      if(err) {
+        console.error('Pipeline failed.', err);
+      }
+    }
+  )
 };
+
+transform()
+
+
