8a8653ef2e5583caf09d0fce80b3a69c71b6ae21
NikitaKakurin <neyasut87@rambler.ru>
2022-06-05 15:29:51 +0300

    fix: cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -5,16 +5,22 @@ export const spawnChildProcess = async (args) => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
   const pathToFile = path.join(__dirname, 'files', 'script.js');    
-  const worker = spawn('node' ,[pathToFile, ...args]  )
+  const child = spawn('node' ,[pathToFile, ...args]  )
 
-  worker.stdout.on('data', (data) => {
-      console.log('stdout: ' + data);
+  child.stdout.on('data', (data) => {
+      console.log('stdout: ' + data.toString().trim());
   });
    
-  process.stdin.on('data', data => worker.stdin.write(data)); 
+  process.stdin.on('data', data => {
+    child.stdin.write(data.toString().trim())
+  }); 
 
-  worker.stderr.on('data',(data) => {
+  child.stderr.on('data',(data) => {
      console.log('stderr: ' + data);
+  });
+
+  child.on('close', (code) => {
+    console.log(`child process exited with code: ${code}`);
   });
 }
 
