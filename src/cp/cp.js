2773bb74bfa9deb1f46b7c3d47903b4218f64e9a
NikitaKakurin <neyasut87@rambler.ru>
2022-06-05 13:37:00 +0300

    feat: add cp.js with spawn

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,22 @@
+import { spawn } from 'child_process'
+import path from 'path';
+import { fileURLToPath } from 'url';
 export const spawnChildProcess = async (args) => {
-    // Write your code here
-};
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const pathToFile = path.join(__dirname, 'files', 'script.js');    
+  const worker = spawn('node' ,[pathToFile, ...args]  )
+
+  worker.stdout.on('data', (data) => {
+      console.log('stdout: ' + data);
+  });
+   
+  process.stdin.on('data', data => worker.stdin.write(data)); 
+
+  worker.stderr.on('data',(data) => {
+     console.log('stderr: ' + data);
+  });
+}
+
+const nodeArgs = process.argv.slice(2);
+spawnChildProcess(nodeArgs);
